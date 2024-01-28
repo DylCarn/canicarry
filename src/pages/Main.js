e5728c30f34b3ed41b8018f6625f0be2ba0ea5a6
DylCarn <23230723+DylCarn@users.noreply.github.com>
2024-01-28 12:21:36 -0600

    Addition of Google Map API language explicitly from it's React App documentation as well as attempt at the marker and resizing.

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+let map, maps;
+
+const handleApiLoaded = (mapLoaded, mapsLoaded) => {
+    map = mapLoaded;
+    maps = mapsLoaded;
+};
 
 const Main = () => {
     const [businesses, setBusinesses] = useState([]);
 
-    //set variable with information returned from PostMan
+    useEffect(() => {
+        const handleResize = () => {
+            if (map && maps) {
+                const center = map.getCenter();
+                maps.event.trigger(map, 'resize');
+                map.setCenter(center);
+            }
+        };
 
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return (
+        <div style={{ height: '100vh', width: '100%' }}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+                defaultCenter={{ lat: 38.884, lng: -94.874}}
+                defaultZoom={16}
+                yesIWantToUseGoogleMapApiInternals
+                onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+            >
+                <AnyReactComponent
+                    lat={59.955413}
+                    lng={30.337844}
+                    text="My Marker"
+                />
+            </GoogleMapReact>
+        </div>
+    );
+};
+
+export default Main;
 
  /*
     useEffect(() => {
@@ -36,16 +78,3 @@ const Main = () => {
         setBusinesses(fetchedBusinesses);
     };
 */
-    return (
-        <div style={{ height: '100vh', width: '100%' }}>
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
-                defaultCenter={{ lat: 38.884, lng: -94.874}}
-                defaultZoom={15}
-                // Add Marker for map..
-            />
-        </div>
-    );
-};
-//
-export default Main;
